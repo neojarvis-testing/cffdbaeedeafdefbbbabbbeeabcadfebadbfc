e54fe3d55740fc9b6cc471e4efa4c288e99452e4
neojarvis <techadmin@iamneo.ai>
2025-02-04 10:48:15 +0000

    Manual-save-commit-04/02/2025--16:18:12

--- project/hotel_functions.py
+++ project/hotel_functions.py
@@ -5,11 +5,56 @@ def load_hotels(filename):
     try:
         with open(filename, 'r') as file:
             return json.load(file)
-    except FileNotFoundError:
+    except Exception:
         return []
 
 # Function to save hotel data to a JSON file
 def save_hotels(hotels, filename):
     with open(filename, 'w') as file:
         json.dump(hotels, file, indent=4)
+
+def add_hotel():
+    hotel_id = int(input())
+    hotel_name = input()
+    hotel_location = input()
+    hotel_rating = float(input())
+    hotel_data = {"ID": hotel_id, "name": hotel_name, "location": hotel_location, "rating": hotel_rating}
+    return hotel_data
+
+def delete_hotel(hotels_):
+    search_hotel_id = int(input("Enter hotel Id"))
+    for hotel in hotels_:
+        if(search_hotel_id == hotel["ID"]):
+            hotels_.remove(hotel)
+            print(f"hotel removed successfully")
+            break
+    else:
+        print("Hotel not found")
+
+def update_rating(hotels_,FILENAME):
+    #hotel id
+    search_hotel_id = int(input("Enter hotel Id"))
+    #hotel new rating
+    new_rating = float(input())
+    #searching for hotel and updating the rating value
+    for hotel in hotels_:
+        if(hotel["ID"] == search_hotel_id):
+            hotel["rating"] = new_rating
+            break
+    save_hotels(hotels_,FILENAME)
+    # else:
+    #     print("Hotel not found")
+
+def fetch_hotel(hotels_):
+    for hotel in hotels_:
+        print( "ID: {}, {}, {}, {}".format(hotel["ID"], hotel["name"], hotel["location"], hotel["rating"]))
+
+def find_hotel(hotels_):
+    search_hotel_id = int(input("Enter hotel Id"))
+    for hotel in hotels_:
+        if(hotel['ID'] == search_hotel_id):
+            print( "ID: {}, {}, {}, {}".format(hotel["ID"], hotel["name"], hotel["location"], hotel["rating"]))
+            break
+    else:
+        print("Hotel not found")
 
--- project/main.py
+++ project/main.py
@@ -1,5 +1,43 @@
 import os
-from hotel_functions import load_hotels, save_hotels
+import io
+from hotel_functions import load_hotels, save_hotels, add_hotel, delete_hotel, update_rating, fetch_hotel, find_hotel
 
 # Define the filename for storing hotel data
-FILENAME = 'hotels.json'
+
+
+
+def main():
+    #filename
+    FILENAME = 'hotels.json'
+    #loading file
+    hotels_ = load_hotels(FILENAME)
+    # menu 
+    menu= ""
+    while(menu!="6"): #runs until menu becomes 6
+        #menu
+        print("1.Add a hotel")
+        print("2.Delete a hotel")
+        print("3.Update hotel rating")
+        print("4.View all hotels")
+        print("5.Search hotels")
+        print("6.Exit")
+        menu = input()
+
+        if(menu=="1"):#add hotel
+            hotels_.append(add_hotel())
+        elif(menu=="2"): #delete hotel
+            delete_hotel(hotels_)
+        elif(menu=="3"): #update hotel rating
+            update_rating(hotels_,FILENAME)
+        elif(menu=="4"): #fetching all hotels
+            fetch_hotel(hotels_)
+        elif(menu=="5"): #search hotels
+            find_hotel(hotels_)
+        elif(menu=="6"):#exit from hotel
+            save_hotels(hotels_,FILENAME)
+            print("Exited")
+            break
+        else:
+            print("Invalid choice")
+
+if __name__=="__main__": main()
